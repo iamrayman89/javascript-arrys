0b78e49a328a2a4b0b59aeb4a9ce1bc6e2cce252
Rayman Moses <raymanmoses89@gmail.com>
2024-09-20 22:24:25 +0300

    second commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const number = [1,2,3];
+
+console.log(number);
+
+// const moreNumber = new Array(5);
+
+// console.log(moreNumber);
+
+// const yetMoreNumbers = Array.of(1,2);
+// console.log(yetMoreNumber);
+
+// const ul = document.querySelectorAll('li');
+// console.log(ul);
+
+// const moreNumbers =  Array.from(ul);
+// console.log(moreNumbers);
+
+// const anlyticsData = [[1,1.5],[6,-9]];
+
+// for (const data of anlyticsData){
+//     for(const dataPoints of data){
+//         console.log(dataPoints);
+//     }
+// }
+
+const hobbies = ['Sport' , 'Cooking'];
+hobbies.push('Reading');
+hobbies.unshift('coding');
+const poppedValue = hobbies.pop();
+hobbies.shift();
+console.log(hobbies);
+console.log(poppedValue);
